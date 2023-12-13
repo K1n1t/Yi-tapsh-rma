3b6ad749b329dfdc31b12e62e713dde7d32816f6
K1n1t <kanatberdimuratov@Kanats-MacBook-Air.local>
2023-12-12 21:39:50 -0600

    NOKOT

--- src/test/java/com/macys/Macys.java
+++ src/test/java/com/macys/Macys.java
@@ -3,5 +3,7 @@ package com.macys;
 public class Macys {
     public static void main(String[] args) {
         System.out.println("Kaanaaat");
+
+        System.out.println("Tirik NOKOT");
     }
 }
