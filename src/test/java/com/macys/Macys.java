42471d699b0e0d736921f163c1481414beaceb5e
K1n1t <kanatberdimuratov@Kanats-MacBook-Air.local>
2023-12-12 22:07:33 -0600

    DELETE ALL

--- src/test/java/com/macys/Macys.java
+++ src/test/java/com/macys/Macys.java
@@ -2,8 +2,6 @@ package com.macys;
 
 public class Macys {
     public static void main(String[] args) {
-        System.out.println("Kaanaaat");
 
-        System.out.println("Tirik NOKOT");
     }
 }
